81b6897f90e9ad99e03a5007ac999bac537636b8
liam9433-coder <liamfarabi@gmail.com>
2025-10-28 12:06:34 +1100

    Stop tracking build directory; add build/ to .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liamsf/robostu1_ws/src/TrailBotPro
-# Build directory: /home/liamsf/robostu1_ws/src/TrailBotPro/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
